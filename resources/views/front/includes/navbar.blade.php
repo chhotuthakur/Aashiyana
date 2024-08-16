437838a3a640f5ed284ff26a3ea46457feea9592
chhotuthakur <npsdahiya@gmail.com>
2024-08-16 20:54:38 +0530

    first commit

---
+++ resources/views/front/includes/navbar.blade.php
@@ -0,0 +1,41 @@
+ <!-- Navbar Start -->
+ <div class="container-fluid nav-bar bg-transparent">
+    <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
+        <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
+            <div class="icon p-1 me-2" >
+                <img class="img-fluid" src="img/logoa.png" alt="Icon" style="width: 60px; height: 60px;">
+            </div>
+            <!-- <h1 class="m-0" style="color:#C33F2C">Aashiyana</h1> -->
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Property</a>
+                    <div class="dropdown-menu rounded-0 m-0">
+                        <a href="property-list.html" class="dropdown-item">Property List</a>
+                        <a href="property-type.html" class="dropdown-item">Property Type</a>
+                    </div>
+                </div>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">More</a>
+                    <div class="dropdown-menu rounded-0 m-0">
+                        <a href="404.html" class="dropdown-item">Contat Us</a>
+                        <a href="404.html" class="dropdown-item">Payment & Cancellation</a>
+                        <a href="404.html" class="dropdown-item">Privacy policy</a>
+                        <a href="404.html" class="dropdown-item">Terms & Conditions </a>
+                    </div>
+                </div>
+                <a href="" class="nav-item nav-link">Package</a>
+            </div>
+            <div class="sign-con" style=" display: flex; gap: 10px;">
+                <a href="/Login.html" class="btn btn-primary px-3 d-none d-lg-flex" style="background-color: #0E2E50;">Sign-up / log-in</a>
+            </div>
+        </div>
+    </nav>
+</div>
+<!-- Navbar End -->
